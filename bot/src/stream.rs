166366ecd398477f00122bc5940a314aa5994834
owobred <29340449+owobred@users.noreply.github.com>
2023-12-04 17:23:47 +0000

    change the samplerate as audio arrives

--- bot/src/stream.rs
+++ bot/src/stream.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use nn::model::ModelContainer;
+use rubato::Resampler;
 use tracing::{debug, info, warn};
 
 const DISCORD_SAMPLERATE_HZ: usize = 48_000;
@@ -70,19 +71,34 @@ fn handle_speech(
     recv: tokio::sync::mpsc::Receiver<Vec<i16>>,
     mut processor: nn::model::SpeakerProcessor,
 ) {
-    let samples = recv.blocking_iter()
-        .flatten()
-        .map(|sample| sample as f32 / i16::MAX as f32)
-        .collect::<Vec<_>>();
-    let samples = samplerate::convert(
-        DISCORD_SAMPLERATE_HZ as u32,
-        WHISPER_SAMPLERATE_HZ as u32,
+    let resample_chunk_size = 20_000;
+
+    let resampler = rubato::FastFixedIn::new(
+        WHISPER_SAMPLERATE_HZ as f64 / DISCORD_SAMPLERATE_HZ as f64,
+        1.0,
+        rubato::PolynomialDegree::Linear,
+        resample_chunk_size,
         1,
-        samplerate::ConverterType::SincFastest,
-        &samples,
     ).unwrap();
 
-    let prediction = processor.predict(samples);
+    let resampled = recv
+        .blocking_iter()
+        .flatten()
+        .map(|sample| sample as f32 / i16::MAX as f32)
+        .collect::<Vec<_>>()
+        .chunks(resample_chunk_size)
+        .map(|chunk| chunk.to_vec())
+        .map(|mut chunk| {
+            if chunk.len() < resample_chunk_size {
+                chunk.resize(resample_chunk_size, 0.0);
+            }
+            chunk
+        })
+        .resample(resampler)
+        .flatten()
+        .collect::<Vec<f32>>();
+
+    let prediction = processor.predict(resampled);
     info!(?prediction, "completed speech");
 
     debug!("done listening");
@@ -107,5 +123,63 @@ trait BlockingMpscIteratorExt<I> {
 impl<I> BlockingMpscIteratorExt<I> for tokio::sync::mpsc::Receiver<I> {
     fn blocking_iter(self) -> BlockingMpscIterator<I> {
         BlockingMpscIterator { inner: self }
+    }
+}
+
+struct ResamplingIterator<I, R>
+where
+    I: Iterator<Item = Vec<f32>>,
+    R: rubato::Resampler<f32>,
+{
+    inner: I,
+    resampler: R,
+    out_buffer: Vec<Vec<f32>>,
+}
+
+impl<I, R> ResamplingIterator<I, R>
+where
+    I: Iterator<Item = Vec<f32>>,
+    R: rubato::Resampler<f32>,
+{
+    fn new(inner: I, resampler: R) -> Self {
+        let out_buffer = resampler.output_buffer_allocate(true);
+        Self {
+            inner,
+            resampler,
+            out_buffer,
+        }
+    }
+}
+
+impl<I, R> Iterator for ResamplingIterator<I, R>
+where
+    I: Iterator<Item = Vec<f32>>,
+    R: rubato::Resampler<f32>,
+{
+    type Item = Vec<f32>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let chunk = self.inner.next()?;
+        self.resampler
+            .process_into_buffer(&[&chunk], &mut self.out_buffer, None)
+            .expect("failed to resample");
+        Some(self.out_buffer[0].clone())
+    }
+}
+
+trait ResamplingIteratorExt<I, R>
+where
+    I: Iterator<Item = Vec<f32>>,
+    R: rubato::Resampler<f32>,
+{
+    fn resample(self, resampler: R) -> ResamplingIterator<I, R>;
+}
+
+impl<I: Iterator<Item = Vec<f32>>, R> ResamplingIteratorExt<I, R> for I
+where
+    R: rubato::Resampler<f32>,
+{
+    fn resample(self, resampler: R) -> ResamplingIterator<I, R> {
+        ResamplingIterator::new(self, resampler)
     }
 }
